6de9b321b61303d4adb2faa47be319b6626746d9
Bobby Hall Jr <bobbyhalljr@Bobbys-MacBook.local>
2019-10-16 13:18:16 -0400

    deleted password being sent back

--- users/userModel.js
+++ users/userModel.js
@@ -8,7 +8,7 @@ module.exports = {
 };
 
 function find() {
-  return db('users').select('id', 'username', 'password', 'Department');
+  return db('users').select('id', 'username', 'Department');
 }
 
 function findBy(filter) {
